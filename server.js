7967057bece589dfda8c74beffd04969d5f4a94c
cmssolutionsgyn@gmailcom <cmssolutionsgyn@gmail.com>
2019-08-26 14:54:29 -0300

    Deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/sysoscli'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(request, response){
   response.sendFile(__dirname + '/dist/sysoscli/index.html');
