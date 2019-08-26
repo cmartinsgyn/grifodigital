4334fe0e2b065f632d8cdb3d5cdd07878fbec92d
cmssolutionsgyn@gmailcom <cmssolutionsgyn@gmail.com>
2019-08-26 15:01:57 -0300

    prod

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(request, response){
-  response.sendFile(__dirname + '/dist/sysoscli/index.html');
+  response.sendFile(__dirname + '/dist/index.html');
 }); 
 
 app.listen(process.env.PORT ||4200);
